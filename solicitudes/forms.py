6e6f510cc5ede0dfbc721782e84888700d9f51a4
mtasigchana-eva2 <mtasigchanam@unemi.edu.ec>
2026-08-19 16:36:45 -0500

    Actualización solicitudes

--- solicitudes/forms.py
+++ solicitudes/forms.py
@@ -6,9 +6,11 @@ class SolicitudForm(forms.ModelForm):
 
     class Meta:
         model = Solicitud
+
         fields = [
             "carrera",
             "sede",
+            "laboratorio",
             "docente",
             "fecha",
             "hora_inicio",
@@ -18,12 +20,51 @@ class SolicitudForm(forms.ModelForm):
         ]
 
         widgets = {
-            "carrera": forms.Select(attrs={"class": "form-select"}),
-            "sede": forms.Select(attrs={"class": "form-select"}),
-            "docente": forms.TextInput(attrs={"class": "form-control"}),
-            "fecha": forms.DateInput(attrs={"type": "date", "class": "form-control"}),
-            "hora_inicio": forms.TimeInput(attrs={"type": "time", "class": "form-control"}),
-            "hora_fin": forms.TimeInput(attrs={"type": "time", "class": "form-control"}),
-            "motivo": forms.Textarea(attrs={"class": "form-control", "rows": 4}),
-            "documento": forms.ClearableFileInput(attrs={"class": "form-control"}),
+            "carrera": forms.Select(
+                attrs={"class": "form-select"}
+            ),
+
+            "sede": forms.Select(
+                attrs={"class": "form-select"}
+            ),
+
+            "laboratorio": forms.TextInput(
+                attrs={"class": "form-control"}
+            ),
+
+            "docente": forms.TextInput(
+                attrs={"class": "form-control"}
+            ),
+
+            "fecha": forms.DateInput(
+                attrs={
+                    "type": "date",
+                    "class": "form-control"
+                }
+            ),
+
+            "hora_inicio": forms.TimeInput(
+                attrs={
+                    "type": "time",
+                    "class": "form-control"
+                }
+            ),
+
+            "hora_fin": forms.TimeInput(
+                attrs={
+                    "type": "time",
+                    "class": "form-control"
+                }
+            ),
+
+            "motivo": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "rows": 4
+                }
+            ),
+
+            "documento": forms.ClearableFileInput(
+                attrs={"class": "form-control"}
+            ),
         }
